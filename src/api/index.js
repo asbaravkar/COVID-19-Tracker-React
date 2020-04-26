7bf599e8aeea04336ff27fa1d1aeb821522845bd
Aditya <aditya.baravkar@gmail.com>
2020-04-26 19:32:53 +0530

    populated countries from api

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import CountryPicker from '../components/CountryPicker/CountryPicker';
 
 const url = 'https://covid19.mathdro.id/api';
 
@@ -26,6 +27,16 @@ export const fetchDailyData = async () =>{
 
         return modifiedData;
     } catch (error) {
+        console.log(error);
+    }
+}
+
+export const fetchCountries = async () => {
+    try {
+        const {data:{countries}} = await axios.get(`${url}/countries`);
         
+        return countries.map((country)=>country.name);
+    } catch (error) {
+        console.log(error);
     }
 }
